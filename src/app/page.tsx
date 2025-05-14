7fb36e868922410d200065de42266079e41e27ac
maxmalato-dell-3567 <max.malato@hotmail.com>
2025-05-14 14:46:54 -0300

    feat(config): GG-3 Configurar o tamanho da fonte H1, H2 e H3 com o tamanho das telas dos dispositivos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,10 @@ export default function Home() {
           </li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 items-center flex-col">
+            <h1>H1 - Título</h1>
+            <h2>H2 - Subtítulos</h2>
+            <h3>H3 - Texto em destaque</h3>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
