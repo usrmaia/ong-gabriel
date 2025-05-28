f4443a8158fab7940b66563efea58946512d2ad7
George Maia <georgemaiaf@gmail.com>
2025-05-27 23:12:06 -0300

    feat(zod): :sparkles: GG-9 Implementar objeto padrão e validado para variáveis de ambiente com Zod

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  }),
 ];
 
 export default eslintConfig;
