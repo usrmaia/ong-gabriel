3d7ade814a69b3928a4f010c304ba214393528eb
George Maia <georgemaiaf@gmail.com>
2025-06-10 21:56:24 -0300

    auth
    Co-Authored-By: Maxjannyfer S. Malato <128427626+maxmalato@users.noreply.github.com>

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import { SignInButton, SignOutButton } from "@/components";
 
 export default function LoginPage() {
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Suporte emocional para a sua vida!</h1>
+      <Image
+        src="/public/gabriel-logo.svg"
+        alt="Logo da ONG Gabriel"
+        width={200}
+        height={200}
+      />
       <SignInButton provider="Google" />
+
       <SignInButton provider="Facebook" />
       <SignOutButton />
     </div>
