13a55c5fbed17c53cba28b51adcce21007d7e161
George Maia <georgemaiaf@gmail.com>
2025-06-25 07:58:54 -0300

    feat(middleware): atualizar configuração de proteção de rotas para permitir autenticação em /api/auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,5 +22,5 @@ export default async function middleware(req: NextRequest) {
 
 // Protect routes
 export const config = {
-  matcher: ["/meu-perfil/:path*", "/api/((?!health).*)"],
+  matcher: ["/meu-perfil/:path*", "/api/((?!auth|health).*)"],
 };
