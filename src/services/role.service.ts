7a08edf62e20b08dc284a039a2c82172b4a2246a
Jorge Luis Gomes Rabelo <110363635+JorgeLuis97@users.noreply.github.com>
2025-07-10 16:16:37 -0300

    refactor(role): simplificar a lógica de verificação e retorno na função addRoleToUser
    Simplifica a função addRoleToUser condensando os retornos e removendo blocos desnecessários, melhorando a legibilidade e a manutenção.

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -12,43 +12,20 @@ export const addRoleToUser = async (
       where: { id: userId },
       select: { id: true, role: true },
     });
+    if (!user)
+      return { success: false, error: { errors: ["Usuário não encontrado!"] }, code: 404 };
 
-    if (!user) {
-      return {
-        success: false,
-        error: { errors: ["Usuário não encontrado!"] },
-        code: 404,
-      };
-    }
-
-    if (user.role.includes(role)) {
-      return {
-        success: true,
-        data: user as User,
-        code: 200,
-      };
-    }
+    if (user.role.includes(role))
+      return { success: true, data: user as User, code: 200 };
 
     const updatedUser = await prisma.user.update({
       where: { id: userId },
-      data: {
-        role: {
-          push: role,
-        },
-      },
+      data: { role: { push: role } },
     });
 
-    return {
-      success: true,
-      data: updatedUser,
-      code: 200,
-    };
+    return { success: true, data: updatedUser, code: 200 };
   } catch (error) {
     logger.error("Erro ao adicionar role ao usuário:", error);
-    return {
-      success: false,
-      error: { errors: ["Erro ao adicionar role ao usuário!"] },
-      code: 500,
-    };
+    return { success: false, error: { errors: ["Erro ao adicionar role ao usuário!"] }, code: 500 };
   }
 };
