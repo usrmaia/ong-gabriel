a033c64524c9d9fe31d52801a47b33c6326f5840
George Maia <georgemaiaf@gmail.com>
2025-07-20 20:21:30 -0300

    feat(proj): :sparkles: OG-27 - Criar tela Home do sistema
    fix(home): 🐛 Corrige a lógica de filtragem de atendimentos e ordenação na página inicial

---
+++ src/app/employee/home/page.tsx
@@ -0,0 +1,85 @@
+import { CalendarDays, ClipboardPlus } from "lucide-react";
+import Link from "next/link";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardPatientAttendance,
+  CardTitle,
+} from "@/components/ui";
+import { PatientAttendance, User } from "@/generated/prisma";
+import { getPatientAttendances } from "@/services";
+import { getUserAuthenticated } from "@/utils/auth";
+
+const CardMenu = (props: {
+  href: string;
+  title: string;
+  icon: React.ReactNode;
+}) => (
+  <Link href={props.href}>
+    <Card className="flex flex-col items-center justify-center gap-2 p-2 w-20 h-24 bg-s-azure-web-100 hover:bg-s-verdigris">
+      <CardHeader className="justify-center text-s-van-dyke">
+        <CardTitle>{props.icon}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardTitle className="font-poppins text-s-liver-100 font-medium text-xs">
+          {props.title}
+        </CardTitle>
+      </CardContent>
+    </Card>
+  </Link>
+);
+
+export default async function HomePage() {
+  const user = await getUserAuthenticated();
+  const isAdmin = user.role.includes("ADMIN");
+  const thirtyMinutesAgo = new Date(new Date().getTime() - 30 * 60 * 1000);
+  const upcomingPatientAttendancesResult = await getPatientAttendances({
+    where: {
+      professionalId: isAdmin ? undefined : { equals: user.id },
+      dateAt: { gte: thirtyMinutesAgo },
+    },
+    include: {
+      patient: true,
+    },
+    orderBy: { dateAt: "asc" },
+  });
+  const upcomingPatientAttendances =
+    (upcomingPatientAttendancesResult.data as (PatientAttendance & {
+      patient: User;
+    })[]) || [];
+
+  return (
+    <>
+      <section className="flex flex-col gap-4">
+        <h3 className="text-center">Boas vindas, {user.name?.split(" ")[0]}</h3>
+        <div className="flex flex-row gap-6">
+          <CardMenu
+            href="/employee/patient/list"
+            title="Pacientes"
+            icon={<ClipboardPlus />}
+          />
+          <CardMenu
+            href="/employee/patient-attendance"
+            title="Agenda"
+            icon={<CalendarDays />}
+          />
+        </div>
+      </section>
+      <section className="flex flex-col gap-2">
+        <p className="font-raleway text-lg font-bold text-s-gunmetal-100">
+          Próximas consultas
+        </p>
+        <div className="flex flex-col gap-2">
+          {upcomingPatientAttendances.map((attendance) => (
+            <CardPatientAttendance
+              attendance={attendance}
+              key={attendance.id}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
