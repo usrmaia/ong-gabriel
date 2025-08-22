2d553872b1e5cc04bac604a34b96b1c2db76b562
George Maia <georgemaiaf@gmail.com>
2025-08-21 22:19:16 -0300

    feat(email): :sparkles: OG-20 - Criar serviço de envio de e-mails com suporte a templates HTML
    refactor(logger): :recycle: Ajustar configuração do transport Console para incluir formatação
    
    refactor(types): :recycle: Ajustar tipo Result para definir valor padrão de T como unknown

---
+++ src/infra/email/template.ts
@@ -0,0 +1,49 @@
+import { readFileSync, existsSync } from "fs";
+import { join } from "path";
+
+import logger from "@/config/logger";
+import { Template } from "./types";
+
+export const getTemplates = (
+  templateName: Template,
+): { templateHTML: string; templateTXT: string } => {
+  const templatesPath = join(process.cwd(), "src", "assets", "templates");
+
+  const templateHTMLPath = join(templatesPath, `${templateName}.html`);
+
+  if (!existsSync(templateHTMLPath)) {
+    logger.error(`Template HTML not found: ${templateHTMLPath}`);
+    throw new Error(`Template HTML not found: ${templateHTMLPath}`);
+  }
+
+  const templateHTML = readFileSync(templateHTMLPath, "utf-8");
+
+  const templateTXTPath = join(templatesPath, `${templateName}.txt`);
+
+  if (!existsSync(templateTXTPath)) {
+    logger.error(`Template TXT not found: ${templateTXTPath}`);
+    throw new Error(`Template TXT not found: ${templateTXTPath}`);
+  }
+
+  const templateTXT = readFileSync(templateTXTPath, "utf-8");
+
+  return { templateHTML, templateTXT };
+};
+
+/**
+ * Cada chave no objeto `context` é usada para substituir todas as ocorrências de `{{key}}`
+ * na string `template` pelo seu valor associado.
+ *
+ * @param template - A string do template contendo espaços reservados no formato `{{key}}`.
+ * @param context - Um objeto que mapeia chaves de espaços reservados para seus valores de string de substituição.
+ * @returns A string com todos os espaços reservados substituídos pelos seus valores correspondentes do `context`.
+ */
+export const applyTemplate = (
+  template: string,
+  context?: Record<string, string>,
+): string => {
+  Object.entries(context || {}).forEach(([key, value]) => {
+    template = template.replace(new RegExp(`{{${key}}}`, "g"), value);
+  });
+  return template;
+};
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import { $ZodErrorTree } from "zod/v4/core";
 
-export type Result<T> = {
+export type Result<T = unknown> = {
   success: boolean;
   data?: T;
   error?: $ZodErrorTree<T>; // { errors: string[]; properties?: Record<string, { errors: string[] }> };
