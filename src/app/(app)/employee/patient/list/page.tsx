4b8dec0ff61b288f1a8111454df22192495c4159
Jorge Luis Gomes Rabelo <110363635+JorgeLuis97@users.noreply.github.com>
2025-08-26 18:05:19 -0300

    refactor(services): :recycle: OG-55 - Criar (e refatorar) componente CardUserAvatar

--- src/app/(app)/employee/patient/list/page.tsx
+++ src/app/(app)/employee/patient/list/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
-
-import { BackNavigationHeader, Card, CardContent } from "@/components/ui";
+import { BackNavigationHeader, CardUserAvatar } from "@/components/ui";
 import { getUsers } from "@/services";
 import { getUserAuthenticated } from "@/utils/auth";
 
@@ -23,28 +20,11 @@ export default async function PatientListPage() {
       <BackNavigationHeader title="Pacientes" href="/employee/home" />
       <section className="grid grid-cols-2 gap-6">
         {patients.map((patient) => (
-          <Link
+          <CardUserAvatar
+            user={patient}
             href={`/employee/patient/details/${patient.id}`}
             key={patient.id}
-          >
-            <Card
-              key={patient.email}
-              className="shadow-lg w-full border-0 rounded-lg h-[200px] p-1 pt-6"
-            >
-              <CardContent className="flex flex-col gap-6">
-                <Image
-                  src={patient.image ?? "/default-user.jpg"}
-                  alt={`${patient.name}'s avatar`}
-                  width={64}
-                  height={64}
-                  className="rounded-full border-1 border-p-tealwave p-0.25"
-                />
-                <p className="text-xl font-medium text-s-van-dyke font-poppins">
-                  {patient.name?.split(" ")?.slice(0, 2)?.join(" ")}
-                </p>
-              </CardContent>
-            </Card>
-          </Link>
+          />
         ))}
       </section>
     </>
--- src/components/ui/user.tsx
+++ src/components/ui/user.tsx
@@ -3,24 +3,50 @@ import { TZDateMini } from "@date-fns/tz";
 import { Mail, Phone } from "lucide-react";
 import { Card, CardContent } from "@/components/ui";
 import { User } from "@prisma/client";
+import Link from "next/link";
+import Image from "next/image";
 
 export const CardUserProfile = ({ user }: { user: User }) => (
-	<Card className="shadow-lg w-full py-4 border-0 rounded-lg">
-		<CardContent className="flex flex-col gap-2 text-s-van-dyke text-sm font-poppins">
-			<div className="flex gap-2">
-				<Mail />
-				<span>{user.email}</span>
-			</div>
-			<div className="flex gap-2">
-				<Phone />
-				<span>{user.phone || "N/A"}</span>
-			</div>
-			<p>
-				<span className="font-bold">Data de Nascimento: </span>
-				{user.date_of_birth
-					? format(new TZDateMini(user.date_of_birth, "UTC"), "dd/MM/yyyy")
-					: "N/A"}
-			</p>
-		</CardContent>
-	</Card>
+  <Card className="shadow-lg w-full py-4 border-0 rounded-lg">
+    <CardContent className="flex flex-col gap-2 text-s-van-dyke text-sm font-poppins">
+      <div className="flex gap-2">
+        <Mail />
+        <span>{user.email}</span>
+      </div>
+      <div className="flex gap-2">
+        <Phone />
+        <span>{user.phone || "N/A"}</span>
+      </div>
+      <p>
+        <span className="font-bold">Data de Nascimento: </span>
+        {user.date_of_birth
+          ? format(new TZDateMini(user.date_of_birth, "UTC"), "dd/MM/yyyy")
+          : "N/A"}
+      </p>
+    </CardContent>
+  </Card>
+);
+
+interface CardUserAvatarProps {
+  user: User;
+  href: string;
+}
+
+export const CardUserAvatar = ({ user, href }: CardUserAvatarProps) => (
+  <Link href={href}>
+    <Card className="shadow-lg w-full border-0 rounded-lg h-[200px] p-1 pt-6">
+      <CardContent className="flex flex-col gap-6">
+        <Image
+          src={user.image ?? "/default-user.jpg"}
+          alt={`${user.name}'s avatar`}
+          width={64}
+          height={64}
+          className="rounded-full border-1 border-p-tealwave p-0.25"
+        />
+        <p className="text-xl font-medium text-s-van-dyke font-poppins">
+          {user.name?.split(" ")?.slice(0, 2)?.join(" ")}
+        </p>
+      </CardContent>
+    </Card>
+  </Link>
 );
