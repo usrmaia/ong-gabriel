356415592523997cec4bba9509d35039fedc0459
Jorge Luis Gomes Rabelo <110363635+JorgeLuis97@users.noreply.github.com>
2025-08-28 13:00:56 -0300

    feat(services): :sparkles: OG-48 - Criar service deleteRoleToUser

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -38,3 +38,38 @@ export const addRoleToUser = async (
     };
   }
 };
+
+export const deleteRoleToUser = async (
+  userId: string,
+  role: Role,
+): Promise<Result<User>> => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { id: true, role: true },
+    });
+    if (!user)
+      return {
+        success: false,
+        error: { errors: ["Usuário não encontrado!"] },
+        code: 404,
+      };
+
+    if (!user.role.includes(role))
+      return { success: true, data: user as User, code: 200 };
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: { role: { set: user.role.filter((r) => r !== role) } },
+    });
+
+    return { success: true, data: updatedUser, code: 200 };
+  } catch (error) {
+    logger.error("Erro ao remover role do usuário:", error);
+    return {
+      success: false,
+      error: { errors: ["Erro ao remover role do usuário!"] },
+      code: 500,
+    };
+  }
+};
