3d0c451b96d552a8e4314b559c49830c08c0f531
Josivania0706 <61471213+Josivania0706@users.noreply.github.com>
2025-09-07 16:55:10 -0300

    feat(pre-psych): ✨ OG-44 - Criar tela de listagem dos candidatos a psicólogo voluntário
    versão 1
    
    componentes
    
    feat(pre-psych): ✨ OG-44 - ajustar consulta para selecionar campos específicos dos psicólogos
    
    feat(permissions): ✨ OG-44 - Adicionar permissão de listagem para psicólogos
    
    feat(pre-psych): ✨ OG-44 - Criar tela de listagem dos candidatos a psicólogo voluntário
    
    versão 1
    
    componentes
    
    feat(pre-psych): ✨ OG-44 - ajustar consulta para selecionar campos específicos dos psicólogos
    
    feat(permissions): ✨ OG-44 - Adicionar permissão de listagem para psicólogos
    
    feat(pre-psych): ✨ OG-44 - Criar tela de listagem dos candidatos a psicólogo voluntário
    
    versão 1
    
    componentes
    
    feat(pre-psych): ✨ OG-44 - ajustar consulta para selecionar campos específicos dos psicólogos
    
    feat(permissions): ✨ OG-44 - Adicionar permissão de listagem para psicólogos
    
    feat(pre-psych): 🆕 Ajustar exibição de CRP e status na lista de psicólogos
    
    Co-Authored-By: Josivânia Pinheiro <61471213+Josivania0706@users.noreply.github.com>

--- src/app/(app)/admin/pre-psych/list/page.tsx
+++ src/app/(app)/admin/pre-psych/list/page.tsx
@@ -8,7 +8,8 @@ import {
   Button,
   Card,
   CardAction,
-  CardContent,
+  CardDescription,
+  CardTitle,
 } from "@/components/ui";
 import { Psych, User } from "@prisma/client";
 import { getPsychs } from "@/services";
@@ -17,94 +18,112 @@ export default async function PrePsychoListPage() {
   const psychresult = await getPsychs({
     select: {
       id: true,
+      CRP: true,
       status: true,
       interviewed: true,
       hasXpSuicidePrevention: true,
-      user: { select: { id: true, name: true, email: true, image: true } },
+      city: true,
+      state: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          full_name: true,
+          email: true,
+          image: true,
+        },
+      },
     },
     where: { status: { in: ["PENDING", "ADJUSTMENT"] } },
     orderBy: { status: "desc" },
   });
 
-  if (!psychresult.success) return <p>Unauthorized</p>;
+  if (!psychresult.success) return <p>{psychresult.error?.errors}</p>;
 
   const psychs = psychresult.data as Array<Psych & { user: User }>;
 
   return (
     <>
       <BackNavigationHeader
-        title="Novos Cadastros à Psicólogo"
+        title="Novos Cadastros de Psicólogo"
         href="/employee/home"
       />
-      <section className="grid grid-cols-1 gap-6">
+      <section className="flex flex-col gap-4">
         {psychs.map((psych) => (
-          <Card
+          <Link
             key={psych.user.email}
-            className="w-full flex flex-row items-center justify-between border-1 border-s-butter-200 rounded-2xl h-50  px-4 py-0"
+            href={`/admin/pre-psych/details/${psych.id}`}
           >
-            <CardContent className="flex flex-col gap-4  bg-amber-400">
-              <div className="flex flex-row gap-3 items-center">
+            <Card className="flex flex-col gap-1 border-s-butter-200 h-44 py-3 px-4 relative">
+              <CardTitle className="flex flex-row gap-3 h-20">
                 <Image
                   src={psych.user.image ?? "/default-user.jpg"}
                   alt={`${psych.user.name}'s avatar`}
-                  width={32}
-                  height={32}
-                  className="rounded-full border-1 border-p-xanthous p-0.25"
+                  width={64}
+                  height={64}
+                  className="rounded-full border-1 border-p-xanthous p-0.25 w-12 h-12"
                 />
 
-                <div>
+                <div className="flex flex-col gap-2">
                   <p className="text-lg font-medium font-poppins">
-                    {psych.user.name?.split(" ")?.slice(0, 2)?.join(" ")}
+                    {psych.user.full_name ?? psych.user.name}
                   </p>
 
                   <div className="flex flex-row gap-4">
-                    <div className="flex flex-row gap-2">
-                      <MapPin color="#AFC6E1" size={16} />
-                      <p className="text-xs font-light text-s-van-dyke font-poppins">
-                        {psych.user.name?.split(" ")?.slice(0, 2)?.join(" ")}
+                    <div className="flex flex-row gap-1">
+                      <UserRound size={16} color="#AFC6E1" />
+                      <p className="text-xs text-nowrap font-light text-s-van-dyke font-poppins">
+                        CRP: {psych.CRP.replace(/^(\d{2})(\d{6})$/, "$1/$2")}
                       </p>
                     </div>
-                    <div className="flex flex-row gap-2">
-                      <UserRound size={16} color="#AFC6E1" />
+                    <div className="flex flex-row gap-1">
+                      <MapPin color="#AFC6E1" size={16} />
                       <p className="text-xs font-light text-s-van-dyke font-poppins">
-                        {psych.user.name?.split(" ")?.slice(0, 2)?.join(" ")}
+                        {psych.city} {psych.state}
                       </p>
                     </div>
                   </div>
                 </div>
-              </div>
+              </CardTitle>
 
-              <div className="flex flex-row gap-4">
-                {psych.hasXpSuicidePrevention && (
-                  <Badge
-                    variant={"outline"}
-                    className="text-success border-success "
-                  >
-                    Experiência
+              <CardDescription className="flex flex-col gap-2">
+                <div className="flex gap-2 w-full">
+                  {psych.hasXpSuicidePrevention && (
+                    <Badge
+                      variant={"outline"}
+                      className="text-success border-success"
+                    >
+                      Com Experiência
+                    </Badge>
+                  )}
+                  {psych.interviewed && (
+                    <Badge
+                      variant={"outline"}
+                      className="text-limited border-limited"
+                    >
+                      Entrevistado
+                    </Badge>
+                  )}
+                </div>
+                {psych.status == "ADJUSTMENT" && (
+                  <Badge className="text-s-charcoal-100 bg-amber-500">
+                    Atualizado
                   </Badge>
                 )}
-                {psych.interviewed && (
-                  <Badge
-                    variant={"outline"}
-                    className="text-limited border-limited "
-                  >
-                    Entrevistado
+                {psych.status == "PENDING" && (
+                  <Badge className="text-s-charcoal-100 bg-available">
+                    Aguardando
                   </Badge>
                 )}
-              </div>
-              <Badge className="text-s-charcoal-100 bg-amber-500 ">
-                Atualizado
-              </Badge>
-            </CardContent>
+              </CardDescription>
 
-            <Link href={`/admin/pre-psych/details/${psych.id}`}>
-              <CardAction>
+              <CardAction className="absolute right-4 top-2/3 transform -translate-y-1/2">
                 <Button className="p-2 rounded-full bg-amber-500 w-10 h-10">
                   <ArrowRight color="#43300E" />
                 </Button>
               </CardAction>
-            </Link>
-          </Card>
+            </Card>
+          </Link>
         ))}
       </section>
     </>
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -63,7 +63,11 @@ function CardAction({ className, ...props }: React.ComponentProps<"div">) {
 
 function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div data-slot="card-content" className={cn("", className)} {...props} />
+    <div
+      data-slot="card-content"
+      className={cn("px-6", className)}
+      {...props}
+    />
   );
 }
 
