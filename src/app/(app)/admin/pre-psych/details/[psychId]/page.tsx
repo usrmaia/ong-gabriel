cfdccb5e0a249903232dcf4add23ed96c22afc15
maxmalato <max.malato@hotmail.com>
2025-09-24 17:20:44 -0300

    feat(pre-psych): ✨ WIP: Atualizar página de detalhes do pré-psicólogo com informações pessoais e residenciais

--- src/app/(app)/admin/pre-psych/details/[psychId]/page.tsx
+++ src/app/(app)/admin/pre-psych/details/[psychId]/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { User, PsychStatus, Psych, Document } from "@prisma/client";
 
-import { BackNavigationHeader, CardUserProfile } from "@/components/ui";
-import { getDocumentById, getPsychById } from "@/services";
+import { BackNavigationHeader, Badge, CardUserProfile } from "@/components/ui";
+import { getPsychById } from "@/services";
 import prisma from "@/lib/prisma";
 
 export default async function PrePsychDetailsPage({
@@ -36,19 +36,107 @@ export default async function PrePsychDetailsPage({
 
   return (
     <>
+      <h2 className="text-center">Cadastro Psicólogo</h2>
+
       <BackNavigationHeader
         title="Pré-Psicólogo"
         href="/admin/pre-psych/list"
       />
+
+      <section className="flex gap-4">
+        <Image
+          src={psych.user.image ?? "/default-user.jpg"}
+          alt={`Avatar de ${psych.user.name}`}
+          width={100}
+          height={100}
+          className="rounded-full border-1 border-p-tealwave p-0.25 w-14 h-14 object-cover"
+        />
+        <div>
+          <p className="font-bold">{psych.user.full_name}</p>
+          <p className="my-1"><span className="font-bold">CRP: </span>{psych.CRP.replace(/^(\d{2})(\d{6})$/, "$1/$2")}</p>
+          <p>{psych.status == "ADJUSTMENT" && (
+            <Badge className="text-s-charcoal-100 bg-amber-500">
+              Atualizado
+            </Badge>
+          )}
+            {psych.status == "PENDING" && (
+              <Badge className="text-s-charcoal-100 bg-available">
+                Aguardando
+              </Badge>
+            )}</p>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-2">
+        <h2 style={{ color: "var(--color-p-xanthous)" }}>Dados Pessoais</h2>
+        <div className="border-b-2 pb-2">
+          <p className="font-bold">Nome</p>
+          <p>{psych.user.full_name}</p>
+        </div>
+        <div className="border-b-2 pb-2">
+          <p className="font-bold">Data de nascimento</p>
+          <p>
+            {psych.user.date_of_birth
+              ? new Date(psych.user.date_of_birth).toLocaleDateString("pt-BR")
+              : "Não informado"}
+          </p>
+        </div>
+        <div className="border-b-2 pb-2">
+          <p className="font-bold">E-mail</p>
+          <p>{psych.user.email.toLowerCase()}</p>
+        </div>
+        <div className="border-b-2 pb-2">
+          <p className="font-bold">Telefone</p>
+          <p>
+            {psych.user.phone
+              ? psych.user.phone.replace(
+                /^(\+?55)?(\d{2})(\d{4,5})(\d{4})$/,
+                "+55 ($2) $3-$4"
+              )
+              : "Não informado"}
+          </p>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-2">
+        <h2 style={{ color: "var(--color-p-xanthous)" }}>Informações Residenciais</h2>
+        <div className="border-b-2 pb-2">
+          <p className="font-bold">CEP</p>
+          <p>{psych.zipCode}</p>
+        </div>
+        <div className="border-b-2 pb-2">
+          <p className="font-bold">Endereço</p>
+          <p>{psych.street}</p>
+        </div>
+        <div className="border-b-2 pb-2 flex gap-8">
+          <div>
+            <p className="font-bold">Número</p>
+            <p>{psych.number}</p>
+          </div>
+          <div className="w-full">
+            <p className="font-bold">Complemento</p>
+            <p>{psych.complement}</p>
+          </div>
+        </div>
+        <div className="border-b-2 pb-2">
+          <p className="font-bold">Bairro</p>
+          <p>{psych.district}</p>
+        </div>
+        <div className="border-b-2 pb-2 flex gap-8">
+          <div>
+            <p className="font-bold">Estado</p>
+            <p>{psych.state}</p>
+          </div>
+          <div className="w-full">
+            <p className="font-bold">Cidade</p>
+            <p>{psych.city}</p>
+          </div>
+        </div>
+      </section>
+
       <section className="flex flex-col gap-2">
         <div className="w-full flex flex-col items-center gap-2">
-          <Image
-            src={psych.user.image ?? "/default-user.jpg"}
-            alt={`Avatar de ${psych.user.name}`}
-            width={100}
-            height={100}
-            className="rounded-full border-1 border-p-tealwave p-0.25 w-14 h-14 object-cover"
-          />
+
           <p className="font-poppins text-lg text-s-van-dyke">
             {psych.user.name}
           </p>
@@ -56,18 +144,18 @@ export default async function PrePsychDetailsPage({
         <CardUserProfile user={psych.user} />
         <h2 className="text-center">Documentos</h2>
         <a
-          href={psych?.curriculumVitae?.id}
+          href={curriculumVitae.id}
           rel="noreferrer"
-          download={psych?.curriculumVitae?.data}
+          download={curriculumVitae.data}
         >
-          {psych?.curriculumVitae?.name ?? "Sem currículo cadastrado."}
+          {curriculumVitae?.name ?? "Sem currículo cadastrado."}
         </a>
         <a
-          href={psych?.proofAddress?.id}
+          href={proofAddress.id}
           rel="noreferrer"
-          download={psych?.proofAddress?.data}
+          download={proofAddress.data}
         >
-          {psych?.proofAddress?.name ?? "Sem endereço cadastrado."}
+          {proofAddress.name ?? "Sem endereço cadastrado."}
         </a>
       </section>
       <div className="mt-6 flex gap-4 justify-center">
