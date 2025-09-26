7e3c2a889caf68657ad38677e3be277644b0998c
George Maia <georgemaiaf@gmail.com>
2025-09-25 22:11:00 -0300

    feat(templates): ✨ OG-49 - Criar templates de email (aprovação/ajuste/reprovação) do candidato a psicólogo voluntário
    fix(email): atualizar template de e-mail de aprovação e corrigir variáveis de ambiente
    
    feat: update email templates and actions for patient attendance
    
    fix(template): atualizar texto e estrutura do template de e-mail de pré-aprovação
    
    fix(template): ajustar estrutura e estilo do template de e-mail de aprovação
    
    fix(template): corrigir variáveis e melhorar texto no template de e-mail de aprovação
    
    feat(email): adicionar variáveis de ambiente para configuração de envio de e-mails e atualizar templates
    
    tamplat-aprovado
    
    Co-Authored-By: Josivânia Pinheiro <61471213+Josivania0706@users.noreply.github.com>

--- src/infra/email/types.ts
+++ src/infra/email/types.ts
@@ -1,11 +1,62 @@
-export type Template = "hello-world";
+import { env } from "@/config/env";
+
+export type Template =
+  | "hello-world"
+  | "pre-psycho-approved"
+  | "pre-psycho-adjustment"
+  | "pre-psycho-failed";
 
 export type TemplateContext = {
   "hello-world": {
     title: string;
   };
+  "pre-psycho-approved": {
+    userName: string;
+  };
+  "pre-psycho-adjustment": {
+    userName: string;
+    pendingNote: string;
+  };
+  "pre-psycho-failed": {
+    userName: string;
+    pendingNote: string;
+  };
+};
+
+export const TemplateContextData: Record<Template, Record<string, string>> = {
+  "hello-world": {},
+  "pre-psycho-approved": {
+    EMAIL_IMG_PUBLIC_URL: env.EMAIL_IMG_PUBLIC_URL,
+    PUBLIC_URL: env.NEXT_PUBLIC_URL,
+    REDIRECT_URL: `${env.NEXT_PUBLIC_URL}/employee/home`,
+    FACEBOOK_URL: "https://www.facebook.com/p/ONG-Gabriel-100083766169010",
+    INSTAGRAM_URL: "https://www.instagram.com/ong.gabriel_",
+    LINKEDIN_URL: "https://www.linkedin.com/company/ong-gabriel",
+    YOUTUBE_URL: "https://www.youtube.com/@ONGGabriel",
+  },
+  "pre-psycho-adjustment": {
+    EMAIL_IMG_PUBLIC_URL: env.EMAIL_IMG_PUBLIC_URL,
+    PUBLIC_URL: env.NEXT_PUBLIC_URL,
+    REDIRECT_URL: `${env.NEXT_PUBLIC_URL}/pre-psych/form-registration`,
+    FACEBOOK_URL: "https://www.facebook.com/p/ONG-Gabriel-100083766169010",
+    INSTAGRAM_URL: "https://www.instagram.com/ong.gabriel_",
+    LINKEDIN_URL: "https://www.linkedin.com/company/ong-gabriel",
+    YOUTUBE_URL: "https://www.youtube.com/@ONGGabriel",
+  },
+  "pre-psycho-failed": {
+    EMAIL_IMG_PUBLIC_URL: env.EMAIL_IMG_PUBLIC_URL,
+    PUBLIC_URL: env.NEXT_PUBLIC_URL,
+    FACEBOOK_URL: "https://www.facebook.com/p/ONG-Gabriel-100083766169010",
+    INSTAGRAM_URL: "https://www.instagram.com/ong.gabriel_",
+    LINKEDIN_URL: "https://www.linkedin.com/company/ong-gabriel",
+    YOUTUBE_URL: "https://www.youtube.com/@ONGGabriel",
+  },
 };
 
 export const TemplateSubject: Record<Template, string> = {
   "hello-world": "Email de Teste - ONG Gabriel",
+  "pre-psycho-approved":
+    "Cadastro Aprovado – Bem-vindo à Plataforma da ONG Gabriel",
+  "pre-psycho-adjustment": "Ajustes Necessários no Cadastro – ONG Gabriel",
+  "pre-psycho-failed": "Cadastro Não Aprovado – ONG Gabriel",
 };
