cb1f4ae4f087a3c47c97643b1eeb815ccb3fe7bd
George Maia <georgemaiaf@gmail.com>
2025-09-25 22:11:00 -0300

    feat(templates): ✨ OG-49 - Criar templates de email (aprovação/ajuste/reprovação) do candidato a psicólogo voluntário
    fix(email): atualizar template de e-mail de aprovação e corrigir variáveis de ambiente
    
    feat: update email templates and actions for patient attendance
    
    fix(template): atualizar texto e estrutura do template de e-mail de pré-aprovação
    
    fix(template): ajustar estrutura e estilo do template de e-mail de aprovação
    
    fix(template): corrigir variáveis e melhorar texto no template de e-mail de aprovação
    
    feat(email): adicionar variáveis de ambiente para configuração de envio de e-mails e atualizar templates
    
    tamplat-aprovado
    
    Co-Authored-By: Josivânia Pinheiro <61471213+Josivania0706@users.noreply.github.com>

--- src/config/env.ts
+++ src/config/env.ts
@@ -13,11 +13,12 @@ const envSchema = z.object({
   AUTH_FACEBOOK_ID: z.string().optional(),
   AUTH_FACEBOOK_SECRET: z.string().optional(),
 
-  EMAIL_GOOGLE_USER: z.string().optional(),
+  EMAIL_GOOGLE_USER: z.string(),
   EMAIL_GOOGLE_ID: z.string().optional(),
   EMAIL_GOOGLE_SECRET: z.string().optional(),
   EMAIL_GOOGLE_REFRESH_TOKEN: z.string().optional(),
   EMAIL_GOOGLE_ACCESS_TOKEN: z.string().optional(),
+  EMAIL_IMG_PUBLIC_URL: z.string().url(),
 
   SECURE_COOKIES_ENABLED: z
     .string()
@@ -29,7 +30,7 @@ const envSchema = z.object({
     .optional()
     .transform((val) => val?.trim().toLowerCase() === "true"),
   NEXT_PUBLIC_HOTJAR_ID: z.string().optional(),
-  NEXT_PUBLIC_URL: z.string().optional(),
+  NEXT_PUBLIC_URL: z.string(),
 
   FEEDBACK_FORM: z.string().url().optional(),
 });
