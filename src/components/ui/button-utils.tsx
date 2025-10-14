a3ea0981b9caf0d0d9efa473aa082eaaef20f94a
usrmaia <georgemaiaf@gmail.com>
2025-10-14 07:44:49 -0300

    fix(proj): :bug: OG-36 - Corrigir botão de retornar para retornar para a última rota do app ou última rota do fluxo

--- src/components/ui/button-utils.tsx
+++ src/components/ui/button-utils.tsx
@@ -1,20 +1,36 @@
+"use client";
+
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import { Button } from "./button";
 
 export const BackNavigationHeader = (props: {
   title: string;
   href: string;
-}) => (
-  <div className="flex items-center w-full">
-    <Link className="flex items-center" href={props.href}>
-      <Button size="icon" variant="ghost">
-        <ChevronLeft />
-      </Button>
-      <p className="font-raleway font-bold text-xl !text-s-taupe-secondary">
-        {props.title}
-      </p>
-    </Link>
-  </div>
-);
+}) => {
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent) => {
+    router.back();
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex items-center w-full">
+      <Link
+        className="flex items-center"
+        href={props.href}
+        onClick={handleClick}
+      >
+        <Button size="icon" variant="ghost">
+          <ChevronLeft />
+        </Button>
+        <p className="font-raleway font-bold text-xl !text-s-taupe-secondary">
+          {props.title}
+        </p>
+      </Link>
+    </div>
+  );
+};
