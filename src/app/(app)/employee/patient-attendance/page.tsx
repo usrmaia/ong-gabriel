2358671397968e7d0cfe3ec3deee664d73bb76ee
usrmaia <georgemaiaf@gmail.com>
2025-10-17 16:57:53 -0300

    fix(patient-attendance): :bug: Corrigir verificação de profissional para incluir usuários sem ID profissional

--- src/app/(app)/employee/patient-attendance/page.tsx
+++ src/app/(app)/employee/patient-attendance/page.tsx
@@ -3,20 +3,17 @@ import { PatientAttendance, User } from "@prisma/client";
 import { BackNavigationHeader } from "@/components/ui";
 import { ListPatientAttendances } from "./list";
 import { getPatientAttendances } from "@/services";
-import { getUserAuthenticated } from "@/utils/auth";
+import { getUserIdAuthenticated } from "@/utils/auth";
 
 export default async function PatientAttendancesPage() {
-  const user = await getUserAuthenticated();
-  const isAdmin = user.role.includes("ADMIN");
+  const userId = await getUserIdAuthenticated();
   const patientAttendancesResult = await getPatientAttendances({
     include: { patient: true },
-    where: { professionalId: isAdmin ? undefined : user.id },
+    where: { OR: [{ professionalId: userId }, { professionalId: null }] },
     orderBy: { dateAt: "asc" },
   });
   const patientAttendances =
-    patientAttendancesResult.data as (PatientAttendance & {
-      patient: User;
-    })[];
+    patientAttendancesResult.data as (PatientAttendance & { patient: User })[];
 
   return (
     <>
