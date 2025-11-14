383cf14894a393bd41b3e8a27e07d683eeb2eec6
George Maia <georgemaiaf@gmail.com>
2025-11-13 22:03:07 -0300

    feat(form-anamnesis): :sparkles: OG-62 - Adicionar restrições ao acessar o formulário de anamneses e ao criar novo atendimento

--- src/app/(app)/patient/form-anamnesis/page.tsx
+++ src/app/(app)/patient/form-anamnesis/page.tsx
@@ -1,8 +1,26 @@
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 import { PatientFormAnamnesis } from "./form";
+import { getPatientFormAnamnesisFromUser } from "@/services";
 
 export default async function PatientFormAnamnesisPage() {
+  const userAnamnesisResult = await getPatientFormAnamnesisFromUser();
+
+  // Caso o usuário tenha preenchido um formulário de anamnese nos últimos 7 dias,
+  // redireciona para a página de falha
+  if (userAnamnesisResult.success) {
+    const lastAnamnesisDate = userAnamnesisResult.data!.reduce(
+      (prev, current) => (prev.createdAt > current.createdAt ? prev : current),
+    ).createdAt;
+
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+    if (lastAnamnesisDate > sevenDaysAgo)
+      redirect("/patient/form-anamnesis/failed");
+  }
+
   return (
     <Suspense fallback={<span>Loading...</span>}>
       <PatientFormAnamnesis />
