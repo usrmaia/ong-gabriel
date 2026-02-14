cc6c01ebdfe56ad0065256f637fc7161e3030f30
George Maia <georgemaiaf@gmail.com>
2026-02-14 19:39:20 -0300

    OG-69 - Permitir que o paciente visualize seu histórico de atendimentos

--- src/app/(app)/patient/patient-attendance/list/page.tsx
+++ src/app/(app)/patient/patient-attendance/list/page.tsx
@@ -1,7 +1,34 @@
-export default async function PatientAttendanceList() {
-    return (
-        <>
-        <h1>Histórico de atendimentos</h1>
-        </>
-    )
- }
+import { PatientAttendance, User } from "@prisma/client";
+
+import { BackNavigationHeader, CardAttendance } from "@/components/ui";
+import { getPatientAttendances } from "@/services";
+import { getUserAuthenticated } from "@/utils/auth";
+
+export default async function PatientAttendanceListPage() {
+  const user = await getUserAuthenticated();
+  const patientAttendancesResult = await getPatientAttendances({
+    include: { patient: true },
+    where: { patientId: user.id },
+    orderBy: { dateAt: "asc" },
+  });
+  const patientAttendances =
+    patientAttendancesResult.data as (PatientAttendance & { patient: User })[];
+
+  return (
+    <>
+      <BackNavigationHeader
+        title="Histórico de atendimentos"
+        href="/patient/home"
+      />
+      <div className="flex flex-col gap-4">
+        {patientAttendances.map((attendance) => (
+          <CardAttendance
+            key={attendance.id}
+            patientAttendance={attendance}
+            mode="patient"
+          />
+        ))}
+      </div>
+    </>
+  );
+}
